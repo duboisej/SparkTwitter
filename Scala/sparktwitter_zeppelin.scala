4d98a98879c7e927adb8151cf3af4ec8c1236fec
Ethan DuBois <edubois@bluemetal.com>
2017-04-10 17:08:36 -0400

    Final changes to spark scala code

--- Scala/sparktwitter_zeppelin.scala
+++ Scala/sparktwitter_zeppelin.scala
@@ -1,10 +1,9 @@
-
-// %livy.spark
+%livy.spark
  
  //The above magic instructs Zeppelin to use the Livy Scala interpreter
 
  // Create an RDD using the default Spark context, sc
- val tweetText = sc.textFile("adl://sparktwitterlakestore.azuredatalakestore.net/twitter/trump_1591835922_41c10d3c66cf46c1a33d05f817f8f9d1.csv")
+ val tweetText = sc.textFile("adl://sparktwitterlakestore.azuredatalakestore.net/twitter/trump.csv")
 
 //adl://<data_lake_store_name>.azuredatalakestore.net/<cluster_root>/HdiSamples/HdiSamples/SensorSampleData/hvac/HVAC.csv
 
@@ -13,18 +12,23 @@
 
  // Map the values in the .csv file to the schema
  
- val tweets = tweetText.map(s => s.split("|")).map(
+ val tweets = tweetText.map(s => s.split('|')).map(
      s => Tweet(s(0), 
              s(1),
              s(2),
              s(3)
      )
  ).toDF()
- 
-
-// print out length of inner array to see if it's just 1 element- could be not matching the pipe character
-
- tweets.show()
 
  // Register as a temporary table called "TrumpTweets"
  tweets.registerTempTable("TrumpTweets")
+
+ %sql
+ // The above magic instructs Zeppelin to use the SQL interpreter
+
+ // Below is an example sql query for the Tweets
+ 
+select sentiment, count(*) as sentimentCount
+from TrumpTweets
+group by sentiment
+order by sentimentCount desc
